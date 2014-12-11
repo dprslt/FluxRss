f2cfba63c08b7a88389783805ad4f91678c9969d
Theo <theo.depresle@gmail.com>
2014-12-11 11:26:30 +0100

    test Twig

--- Vue/vueClass.php
+++ Vue/vueClass.php
@@ -5,6 +5,17 @@
  * Date: 10/12/2014
  * Time: 19:24
  */
+    include_once('../lib/Twig/Autoloader.php');
+    Twig_Autoloader::register();
+
+    $loader = new Twig_Loader_Filesystem('Templates'); // Dossier contenant les templates
+    $twig = new Twig_Environment($loader, array(
+        'cache' => false
+    ));
+
+
+    $template = $twig->loadTemplate('test.twig');
+
 ?>
 <!DOCTYPE HTML>
 <html>
@@ -15,7 +26,11 @@
 
     <body>
         <div class="top_bar" >
-
+            <?=
+            $template->render(array(
+                'moteur_name' => 'Twig'
+            ))
+                ?>
         </div>
         <div class="navig_bar">
             <p>Menu</p>
@@ -40,6 +55,5 @@
 
             </div>
         </div>
-
     </body>
 </html>
