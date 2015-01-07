e2534c440068b4895900e7d8ba530f03cfcc7ec9
MsRager <delain.charlotte@gmail.com>
2015-01-07 11:03:58 +0100

    Partie Admin

--- Controleur/adminControl.php
+++ Controleur/adminControl.php
@@ -8,17 +8,40 @@
 
 namespace controleur;
 
+use modele\AdminModele;
 use modele\FluxModele;
 use utilitaires\Validation;
+use Twig_Autoloader;
+use Twig_Environment;
+use Twig_Loader_Filesystem;
 
 class adminControl{
+    private $twig;
+    private $fluxModele;
+    private $adminModele;
+    
     public function __construct(){
         global $path;
-        switch($_REQUEST['action']){
-            case 'rafraichirNews':
-                require($path.'refreshRSSContent.php');
+        
+        $this->fluxModele = new FluxModele();
+        $this->adminModele = new AdminModele();
+        
+        Twig_Autoloader::register();
+        $loader = new Twig_Loader_Filesystem('Vue/Templates');
+        $this->twig = new Twig_Environment($loader, array(
+            'cache' => false
+        ));
+        
+        $action = (isset($_REQUEST['action'])?$_REQUEST['action']:null);
+        switch($action){
+            case 'pageConnexion':
+                $this->AfficherPageConnexion();
+                break;
+            case 'connexion':
+                $this->connexion();
                 break;
         }
+        
     }
 
     function addFlux()
@@ -30,5 +53,17 @@ class adminControl{
 
         $fluxMod = new FluxModele();
         $fluxMod->ajouterFlux($link);
+    }
+    
+    function AfficherPageConnexion(){
+        $fluxs = $this->fluxModele->getPageFlux(1);
+        $template = $this->twig->loadTemplate('pageConnexion.twig');
+        echo $template->render(array(
+            'Fluxs' => $fluxs,
+                ));
+    }
+    
+    function connexion(){
+    $connexion = $this->adminModele->connecter($_POST['login'],$_POST['mdp']);
     }
 }
